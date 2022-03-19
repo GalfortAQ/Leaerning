61c2c35b88d259ee7a23dc379dc70a0fed8948f3
GalfortAQ <alexquid_galfort@hotmail.com>
2022-03-18 19:38:43 -0500

    First Update

---
+++ app/src/main/java/com/vmuguerzac/vuforiastarter/ActivityLauncher.java
@@ -0,0 +1,29 @@
+package com.vmuguerzac.vuforiastarter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class ActivityLauncher extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_launcher);
+    }
+
+    public void onIniciarClick(View view) {
+        Intent i = new Intent(ActivityLauncher.this, ImageTargets.class);
+        startActivity(i);
+
+    }
+}
